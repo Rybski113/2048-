de67577ccf8050c144f401d44485df9568d95696
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-06-23 07:27:07 +0200

    adding moveUp and moveDown functions

--- app.js
+++ app.js
@@ -99,15 +99,40 @@ document.addEventListener('DOMContentLoaded', () => {
         let column = [parseInt(totalOne), parseInt(totalTwo),parseInt(totalThree), parseInt(totalFour)]
 
         let filteredColumn = column.filter(num=> num)
+        let missing = 4 - filteredColumn.length
+        let zeros = Array(missing).fill(0)
+        let newColumn = zeros.concat(filteredColumn)
+
+        squares[i].innerHTML = newColumn[0]
+        squares[i+width].innerHTML = newColumn[1]
+        squares[i+(width*2)].innerHTML = newColumn[2]
+        squares[i+(width*3)].innerHTML = newColumn[3]
+
     }
   }
 
+  // swipe up
 
+  function moveUp() {
+    for (let i=0; i < 4; i++) {
+        let totalOne = squares[i].innerHTML
+        let totalTwo = squares[i+width].innerHTML
+        let totalThree = squares[i+(width*2)].innerHTML
+        let totalFour = squares[i+(width*3)].innerHTML
+        let column = [parseInt(totalOne), parseInt(totalTwo),parseInt(totalThree), parseInt(totalFour)]
 
+        let filteredColumn = column.filter(num=> num)
+        let missing = 4 - filteredColumn.length
+        let zeros = Array(missing).fill(0)
+        let newColumn = filteredColumn.concat(zeros)
 
+        squares[i].innerHTML = newColumn[0]
+        squares[i+width].innerHTML = newColumn[1]
+        squares[i+(width*2)].innerHTML = newColumn[2]
+        squares[i+(width*3)].innerHTML = newColumn[3]
 
-
-
+    }
+  }
 
 
    function combineRow() {
@@ -119,6 +144,17 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       }
    }
+
+
+   function combineColumn() {
+    for ( let i=0; i < 12; i++) {
+      if (squares[i].innerHTML === squares[i+width].innerHTML) {
+          let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i+width].innerHTML)
+          squares[i].innerHTML = combinedTotal
+          squares[i+width].innerHTML = 0
+      }
+    }
+ }
 
    //assign keycodes
     
